5e9abaffb1f123b54dc9843fce85a64ecb9f80f3
Trinh Tuan Tai <tuantai229@M2Pro.local>
2024-11-15 15:14:26 +0700

    week6 car.csv regression and classification

--- resource/classification.py
+++ resource/classification.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import accuracy_score, f1_score, classification_report
+import os
 
-data = pd.read_csv("diabetes.csv")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "diabetes.csv")
+data = pd.read_csv(file_path)
 # profile = ProfileReport(data, title="Diabetes Report", explorative=True)
 # profile.to_file("report.html")
 target = "Outcome"
@@ -21,8 +24,19 @@ scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-model = LogisticRegression(random_state=100)
+# model = LogisticRegression(random_state=100)
+# model.fit(x_train, y_train)
+
+params = {
+    "n_estimators": [50, 100, 200, 500],
+    "criterion": ["gini", "entropy", "log_loss"],
+    "max_depth": [None, 2, 5, 10]
+}
+model = GridSearchCV(RandomForestClassifier(random_state=100), param_grid=params, scoring="recall", cv=6, verbose=2, n_jobs=4)
 model.fit(x_train, y_train)
+
+print("Best score: {}".format(model.best_score_))
+print("Best param: {}".format(model.best_params_))
 
 y_predict = model.predict(x_test)
 print(classification_report(y_test, y_predict))
